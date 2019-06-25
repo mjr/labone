06de3a78181798463c17a014bd4d95d93a3562e2
Manaia Junior <manaiajr.23@gmail.com>
2019-06-25 10:20:33 -0300

    Update project

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Desenvolvimento, modularização, depuração e profiling de programas em C++.
 
-Alunos: Lucas Santos Neves e Roberto Manaia dos Santos Junior
+Alunos: **Lucas Santos Neves e Roberto Manaia dos Santos Junior**
 
 ## Como compilar e rodar cada programa?
 
@@ -16,8 +16,45 @@ make
 ./bin/main --options
 ```
 
-1. Segunda questão.
+1. Entre na pasta do programa (ex: question_two).
+2. Compile os arquivos.
+3. Execute o programa.
 
 ```console
+cd question_two/
 g++ -o main -O0 main.cpp
+./main
+```
+
+## Observações
+
+Para não dar um erro no cálculo de N nas séries de Taylor, colocamos um valor máximo para que não seja possível iterar mais de 500 vezes (só para não causar overflow no programa).
+
+Para depurar o nosso programa utilizando o `gdb` podemos rodar os seguintes comandos:
+
+### Compilando o programa com as diretivas de compilação -g e -O0, necessárias para o uso do `gdb`.
+### Executar o programa no ambiente `gdb`
+
+```console
+g++ -o main -g -O0 main.cpp
+gdb main
+ ```
+
+Agora que estamos no `prompt` do `gdb`, podemos utilizar os comandos do `gdb`:
+
+### Adicionando o ponto de parada na linha 59 (para inspecionar assim que a condição dos casos a-c forem satisfeitas)
+
+```console
+break 59
+ ```
+
+Para analisar o nosso programa utilizando o `gprof` podemos rodar os seguintes comandos:
+
+### Compilando o programa com a diretiva de compilação -pg, necessárias para que o compilador insira mais informações para o profiler
+### O profile gmon.out será criado
+### Executando o `gprof` para exibir as informações sobre as chamadas das funções ao longo da execução, utilizando a opção `call graph`
+
+```console
+g++ -o main -pg -O0 main.cpp
+gprof --brief -q main
 ```
--- question_two/main.cpp
+++ question_two/main.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 using namespace std;
 
-int factorial(int n) {
-    int factorial = 1;
+long int factorial(int n) {
+    long int factorial = 1;
 
     for (int i = 1; i <= n; ++i) {
         factorial *= i;
@@ -13,41 +13,78 @@ int factorial(int n) {
     return factorial;
 }
 
-double _sin_precision(double x, int k) {
-    double _sin_precision = 0;
-    int aux;
-    for (int i = 0; i <= k; ++i) {
+int check_precision(long double diff, double value) {
+    if (diff <= value) {
+      return 1;
+    } else {
+      return -1;
+    }
+}
+
+long double _sin_precision(long double x) {
+    long double _sin_precision = 0;
+    long double _sin_precision_temp = 0;
+    int aux, check_a, check_b, check_c;
+    // int temp;
+
+    int i = 1;
+    do {
         aux = (i * 2) + 1;
         if (aux >= 3) {
           // cout << ">>> " << x;
-          // cout << " | " << pow(-1, i);
+          // temp = pow(-1, i);
+          //
+          // if (temp == -1)
+          //     cout << " | " << temp;
+          // else
+          //     cout << " |  " << temp;
           // cout << " * (" << x << "^" << aux;
           // cout << " / " << aux << "!)" << endl;
 
           _sin_precision += pow(-1, i) * (pow(x, aux) / factorial(aux));
+          _sin_precision_temp = x + _sin_precision;
+
+          check_a = check_precision(pow((_sin_precision_temp - sin(x)), 2), 0.0001);
+          check_b = check_precision(pow((_sin_precision_temp - sin(x)), 2), 0.00001);
+          check_c = check_precision(pow((_sin_precision_temp - sin(x)), 2), 0.000001);
+
+          // cout << " ->> a | " << check_a << endl;
+          // cout << " ->> b | " << check_b << endl;
+          // cout << " ->> c | " << check_c << endl;
+          // cout << "N = " << i << " (O menor valor de N para que a, b e c sejam atendidas se possível)" << endl;
         }
-    }
+        i++;
+    } while((check_a != 1 || check_b != 1 || check_c != 1) && (i <= 500));
+
+    if (i <= 500)
+        cout << "N = " << i << " (O menor valor de N para que a, b e c sejam atendidas se possível)" << endl;
+    else
+        cout << "Não foi possível calcular o N, por conta do limite adicionado ao código (o limite é 500 iterações) para não acontecer overflow no programa." << endl;
 
     return x + _sin_precision;
 }
 
-double _sin(double x) {
-    return x - (pow(x,3) / 6) + (pow(x,5) / 120);
+long double _sin(long double x) {
+    return x - (pow(x, 3) / 6) + (pow(x, 5) / 120);
 }
 
-double _cos(double x) {
-    return 1 - (pow(x,2) / 2) + (pow(x,4) / 24);
+long double _cos(long double x) {
+    return 1 - (pow(x, 2) / 2) + (pow(x, 4) / 24);
 }
 
 int main(int argc, char const *argv[]) {
-    double value;
+    long double value, result;
+
     cout << "Digite um ângulo (em radianos): ";
     cin >> value;
-    cout << "<cmath seno>: " << sin(value) << endl;
-    cout << "<imple seno>: " << _sin(value) << endl;
-    cout << "<cmath cosseno>: " << cos(value) << endl;
+
+    result = _sin_precision(value);
+
     cout << "<imple cosseno>: " << _cos(value) << endl;
-    cout << "<imple taylor seno>: " << _sin_precision(value, 5) << endl;
+    cout << "<cmath cosseno>: " << cos(value) << endl;
+    cout << "<imple seno>: " << _sin(value) << endl;
+    cout << "<cmath seno>: " << sin(value) << endl;
+    cout << "<imple taylor seno>: " << result << endl;
 
     return 0;
 }
